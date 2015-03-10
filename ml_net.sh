b37c5769ca578b7fb2431d416735319ac2986a7c
Joseph Mingrone <jrm@ftfl.ca>
2015-03-10 16:09:06 -0300

    Remove reliance on driver-specific sysctl nodes

--- ml_net.sh
+++ ml_net.sh
@@ -2,27 +2,40 @@
 
 dri=`/usr/bin/netstat -r4n | awk '/^default/ {print $4}'`
 dri_dn=`echo ${dri} | sed -E 's/([0-9]+)/.\1/'`
-dr=-1
-dt1=`now` && d1='-1'
-dt2='0' && d2='0'
+[ -n "$dri" ] && t1=`now` && \
+	rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | awk 'NR%3==0 {print $8 " " $11}'`
+set -- ${rate_info}
+[ -n "$1" ] && d1=$1; d2='0'; dr=-1
+[ -n "$2" ] && u1=$2; u2='0'; ur=-1
+t2='0'
 interval=3
-ur=-1
-ut1=`now` && u1='-1'
-ut2='0' && u2='0'
+
 stump_pid=`pgrep -a -n stumpwm`
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    dri=`/usr/bin/netstat -r4n | awk '/^default/ {print $4}'`
-    dri_dn=`echo ${dri} | sed -E 's/([0-9]+)/.\1/'`
-    dt2=`now` && \
-	d2=`/sbin/sysctl -n dev.${dri_dn}.mac_stats.good_octets_recvd 2>/dev/null` &&\
-	dr=`echo "scale=2; (${d2} - ${d1}) / (${dt2} - ${dt1}) / 1024" | bc`
-    ut2=`now` && \
-	u2=`/sbin/sysctl -n dev.${dri_dn}.mac_stats.good_octets_txd 2>/dev/null` &&\
-	ur=`echo "scale=2; (${u2} - ${u1}) / (${ut2} - ${ut1}) / 1024" | bc`    
-    printf "%s\t%8.2f\t%8.2f\n" ${dri} ${ur} ${dr}
-    dt1=${dt2} && d1=${d2}
-    ut1=${ut2} && u1=${u2}
+    dri=`/usr/bin/netstat -r4n 2>/dev/null | awk '/^default/ {print $4}'`
+    if [ -n "${dri}" ]; then
+	dri_dn=`echo ${dri} | sed -E 's/([0-9]+)/.\1/'`
+	t2=`now` &&\
+	    rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | awk 'NR%3==0 {print $8 " " $11}'`
+	set -- ${rate_info}
+	[ -n "$1" ] && d2=$1;
+	[ -n "$2" ] && u2=$2;
+	dr=`echo "scale=2; (${d2} - ${d1}) / (${t2} - ${t1}) / 1024" | bc`
+	ur=`echo "scale=2; (${u2} - ${u1}) / (${t2} - ${t1}) / 1024" | bc`
+	printf "%s\t" ${dri}
+ 	case $ur in
+	    ''|*[!0-9]*) printf "%8.2f\t" ${ur};;
+	    *) printf "-1";;
+	esac
+	case $dr in
+	    ''|*[!0-9]*) printf "%8.2f\n" ${dr};;
+	    *) printf "-1\n";;
+	esac
+	t1=${t2}; d1=${d2}; u1=${u2}
+    else
+	printf "\-\t-1\t-1\n"
+    fi
     sleep ${interval}
 done
