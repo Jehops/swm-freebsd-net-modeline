25b7e5bbf96be05f689b4a6f960fa150c799ae3a
Joseph Mingrone <jrm@ftfl.ca>
2015-03-10 21:54:07 -0300

    Make the script robust against errors and changes in interface state.

--- ml_net.sh
+++ ml_net.sh
@@ -2,10 +2,11 @@
 
 dri=`/usr/bin/netstat -r4n | awk '/^default/ {print $4}'`
 [ -n "$dri" ] && t1=`now` && \
-	rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | awk 'NR%3==0 {print $8 " " $11}'`
+	rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | \
+awk 'NR%3==0 {print $8 " " $11}'`
 set -- ${rate_info}
-[ -n "$1" ] && d1=$1; d2='0'; dr=-1
-[ -n "$2" ] && u1=$2; u2='0'; ur=-1
+[ -n "$1" ] && d1=$1; d2='0'; dr=-1.0
+[ -n "$2" ] && u1=$2; u2='0'; ur=-1.0
 t2='0'
 interval=3
 
@@ -16,7 +17,8 @@ while kill -0 $stump_pid > /dev/null 2>&1; do
     dri=`/usr/bin/netstat -r4n 2>/dev/null | awk '/^default/ {print $4}'`
     if [ -n "${dri}" ]; then
 	t2=`now` &&\
-	    rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | awk 'NR%3==0 {print $8 " " $11}'`
+	    rate_info=`/usr/bin/netstat -I ${dri} -q 1 -b 2>/dev/null | \
+awk 'NR%3==0 {print $8 " " $11}'`
 	set -- ${rate_info}
 	[ -n "$1" ] && d2=$1;
 	[ -n "$2" ] && u2=$2;
@@ -24,16 +26,16 @@ while kill -0 $stump_pid > /dev/null 2>&1; do
 	ur=`echo "scale=2; (${u2} - ${u1}) / (${t2} - ${t1}) / 1024" | bc`
 	printf "%s\t" ${dri}
  	case $ur in
-	    ''|*[!0-9]*) printf "%8.2f\t" ${ur};;
-	    *) printf "-1";;
+	    ''|*.*.*|*[!0-9.]*) printf "%8.2f\t" "-1.0";;
+	    *) printf "%8.2f\t" ${ur};;
 	esac
 	case $dr in
-	    ''|*[!0-9]*) printf "%8.2f\n" ${dr};;
-	    *) printf "-1\n";;
+	    ''|*.*.*|*[!0-9.]*) printf "%8.2f\n" "-1.0";;
+	    *) printf "%8.2f\n" ${ur};;
 	esac
 	t1=${t2}; d1=${d2}; u1=${u2}
     else
-	printf "\-\t-1\t-1\n"
+	printf "\-\t-1.0\t-1.0\n"
     fi
     sleep ${interval}
 done
