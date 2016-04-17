008d9955563a4fc05ff8bae12177f8572a903042
Joseph Mingrone <jrm@ftfl.ca>
2016-04-17 13:47:39 -0300

    trivial

--- ml_net.sh
+++ ml_net.sh
@@ -4,7 +4,7 @@ dri=$(/usr/bin/netstat -r4n | awk '/^default/ {print $4}')
 [ -n "$dri" ] && t1=$(now) && \
     rate_info=$(/usr/bin/netstat -I "$dri" -q 1 -b 2>/dev/null | \
                     awk 'NR%3==0 {print $8 " " $11}')
-set -- ${rate_info}
+set -- $rate_info
 [ -n "$1" ] && d1=$1; d2='0'; dr=-1.0
 [ -n "$2" ] && u1=$2; u2='0'; ur=-1.0
 t2='0'
@@ -19,11 +19,11 @@ while kill -0 "$stump_pid" > /dev/null 2>&1; do
 	t2=$(now) && \
 	    rate_info=$(/usr/bin/netstat -I "$dri" -q 1 -b 2>/dev/null | \
                             awk 'NR%3==0 {print $8 " " $11}')
-	set -- ${rate_info}
+	set -- $rate_info
 	[ -n "$1" ] && d2=$1;
 	[ -n "$2" ] && u2=$2;
-	dr=$(echo "scale=2; (${d2} - ${d1}) / (${t2} - ${t1}) / 1024" | bc)
-	ur=$(echo "scale=2; (${u2} - ${u1}) / (${t2} - ${t1}) / 1024" | bc)
+	dr=$(echo "scale=2; ($d2 - $d1) / ($t2 - $t1) / 1024" | bc)
+	ur=$(echo "scale=2; ($u2 - $u1) / ($t2 - $t1) / 1024" | bc)
 	printf "%s\t" "$dri"
  	case "$ur" in
 	    ''|*.*.*|*[!0-9.]*) printf "%8.2f\t" "-1.0";;
